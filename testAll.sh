420523f0d1247c1b422bf9ac0981b352807e23f0
Hikmat Farhat <62365718+hikmatfarhat-ndu@users.noreply.github.com>
2021-02-09 19:01:02 +0200

    Update testAll.sh

--- testAll.sh
+++ testAll.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "BOOTS ${BOOST_ROOT_1_72_0} END"
-rm -rf build
-mkdir build
 cd build
-cmake .. -DBoost_INCLUDE_DIR=$BOOST_ROOT_1_72_0/include
-cmake --build . --config Release
 ./FaChecker ../problems.json
