701c5b0571f8dad07b8f4de5e392bdd57f9e65f0
Hikmat Farhat <62365718+hikmatfarhat-ndu@users.noreply.github.com>
2021-02-09 18:22:21 +0200

    Update testAll.sh

--- testAll.sh
+++ testAll.sh
@@ -2,6 +2,6 @@
 rm -rf build
 mkdir build
 cd build
-cmake -DBoost_INCLUDE_DIR=$BOOST_ROOT_1_72_0/include -DCMAKE_BUILD_TYPE=Release ..
+cmake .. -DBoost_INCLUDE_DIR=$BOOST_ROOT_1_72_0/include
 cmake --build . --config Release
 ./FaChecker ../problems.json
