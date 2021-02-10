162820045a34bc170f5540bb2d2e869bdc975fbf
hikmat farhat <hfarhat@ndu.edu.lb>
2021-02-10 18:17:42 +0200

    tweaks

--- FaChecker.cpp
+++ FaChecker.cpp
@@ -28,7 +28,7 @@ std::string report(const json& tree) {
 				STREAM << " PASSED" << "\tPoints=" << t["points"] << "\n";
 			}
 		}
-		STREAM << "SUBTOTAL=   " << p["total"] << "/" << p["max points"] << "\n\n";
+		STREAM << "SUBTOTAL=   " << p["total"] << "/" << p["max_points"] << "\n\n";
 
 	}
 	return STREAM.str();
@@ -75,10 +75,10 @@ void runTests(json& p,std::string parent_path) {
 	json& tests = p["tests"];
 	bool flip = false;
 	int total = 0;
-	int possible = 0;
+	//int possible = 0;
 	
 	for (auto& t : tests) {
-		possible += static_cast<int> (t["points"]);
+	//	possible += static_cast<int> (t["points"]);
 		bool r = nfa.accept(t["input"]);
 		bool er = t["expected-output"];
 		if (r != er) {
@@ -91,7 +91,10 @@ void runTests(json& p,std::string parent_path) {
 	
 	}
 	p["total"] = total;
-	p["max_points"] = possible;
+	if(debug){
+		std::cerr<<"subtotal="<<p["total"]<<"/"<<p["max_points"]<<"\n";
+	}
+	
 
 }
 
@@ -117,6 +120,7 @@ json  runProblems(std::string filename) {
 	}
 	if(debug)std::cerr<<"Processing problems\n";
 	json tree = json::parse(input);
+	/* retrieve all problems */
 	json& problems = tree["problems"];
 	int max_possible=0;
 	int total = 0;
@@ -124,15 +128,15 @@ json  runProblems(std::string filename) {
 /* first scan and compute max possible points
  * before they change
  */
-	int p_max_point = 0;
-	for(auto& p:problems){
-		for(auto t:p["tests"]){
-			max_possible+=static_cast<int>(t["points"]);
-			p_max_point += static_cast<int>(t["points"]);
-		}
-		p["max_points"] = p_max_point;
-		p_max_point = 0;
-	}
+	// int p_max_point = 0;
+	// for(auto& p:problems){
+	// 	for(auto t:p["tests"]){
+	// 		max_possible+=static_cast<int>(t["points"]);
+	// 		p_max_point += static_cast<int>(t["points"]);
+	// 	}
+	// 	p["max_points"] = p_max_point;
+	// 	p_max_point = 0;
+	// }
 	for (auto& p : problems) {
 		/* make sure the entry p["total"]
 		* exists in case test fails
@@ -142,7 +146,7 @@ json  runProblems(std::string filename) {
 		total += static_cast<int>(p["total"]);
 	}
 	tree["total"] = total;
-	tree["max_points"]=max_possible;
+	//tree["max_points"]=max_possible;
 	return tree;
 	
 }
