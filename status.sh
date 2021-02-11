64c8f73284a6702a3445785f142c2e0b2e21b865
Hikmat Farhat <62365718+hikmatfarhat-ndu@users.noreply.github.com>
2021-02-11 06:52:02 +0200

    Update status.sh

--- status.sh
+++ status.sh
@@ -2,9 +2,11 @@ max=$(head -1 report.md|cut -d ' ' -f 3|cut -d '/' -f 2)
 total=$(head -1 report.md|cut -d ' ' -f 3|cut -d '/' -f 1)
 echo $total
 echo $max
-if ! [ "$GITHUB_ACTOR" = "hikmatfarhat" ]
+if ! [ "$GITHUB_ACTOR" = "github-classroom" ]
 then
 gh pr comment 1 -b "Total: $total / $max "
+else
+  echo "ACTOR = $GITHUB_ACTOR "
 fi
 
 if [ "$total" -lt "$max" ]
