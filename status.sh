f76e8ddb8a31dfb622268903a291ded3b49f131c
Hikmat Farhat <62365718+hikmatfarhat-ndu@users.noreply.github.com>
2021-09-16 05:37:28 +0300

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,15 +1,16 @@
 max=$(head -1 report.md|cut -d ' ' -f 3|cut -d '/' -f 2)
 total=$(head -1 report.md|cut -d ' ' -f 3|cut -d '/' -f 1)
-echo $total
-echo $max
-echo "ACTOR = $GITHUB_ACTOR "
+echo "Points ${total}/${max}"
+#echo $total
+#echo $max
+#echo "ACTOR = $GITHUB_ACTOR "
 
-echo "RUN NUMBER= $GITHUB_RUN_NUMBER"
+#echo "RUN NUMBER= $GITHUB_RUN_NUMBER"
 
 #gh pr comment 1 -b "Total: $total / $max "
 
-if [ "$total" -lt "$max" ]
-then
-	exit 1
-fi
+#if [ "$total" -lt "$max" ]
+#then
+#	exit 1
+#fi
 
